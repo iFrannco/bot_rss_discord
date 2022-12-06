ddf9e130b0657352808de018d3441aef91fc82de
franco <fcabeza91@gmail.com>
2022-12-06 19:06:27 -0300

    importar la libreria dotenv

--- bot_sender.py
+++ bot_sender.py
@@ -1,9 +1,10 @@
 from functions import new_entries_db, update_entry
 import requests
 import os
+from dotenv import load_dotenv
 
-# usar una variable de entorno para elegir el chat donde va a funcionar el bot
 
+load_dotenv()
 discord_bot = os.environ.get("webhook_key")
 new_entries = new_entries_db()
 
